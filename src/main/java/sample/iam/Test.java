57abfd7cbf1654761d06f9026b0890bb456b898d
gomesuit <gomesuit@gmail.com>
2016-08-07 21:21:43 +0900

    sort

--- src/main/java/sample/iam/Test.java
+++ src/main/java/sample/iam/Test.java
@@ -1,7 +1,9 @@
 package sample.iam;
 
-import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
@@ -22,12 +24,12 @@ import net.gpedro.integrations.slack.SlackMessage;
 public class Test {
 
 	public static void main(String[] args) {
-		if(args.length != 1){
+		if (args.length != 1) {
 			return;
 		}
-		
+
 		String webHook = args[0];
-		String message = "";
+		List<Message> messages = new ArrayList<>();
 
 		AWSCredentialsProvider cp = new EnvironmentVariableCredentialsProvider();
 		AmazonIdentityManagement iam = new AmazonIdentityManagementClient(cp);
@@ -51,28 +53,113 @@ public class Test {
 				System.out.println("key : " + key.getAccessKeyId());
 				System.out.println("used : " + result.getAccessKeyLastUsed().getLastUsedDate());
 
-				message = message + getMessageRow(key, result.getAccessKeyLastUsed());
+				messages.add(createMessage(key, result.getAccessKeyLastUsed()));
 			}
 		}
 		// sendSlack(webHook, message);
-		System.out.println(message);
+		printSlack(messages);
 	}
 
-	private static void sendSlack(String webHook, String message) {
+	private static void printSlack(List<Message> messages) {		
+		String msg = "";
+		Collections.reverse(messages);
+		for (Message message : messages) {
+			msg = msg + formatMessage(message);
+		}
+		System.out.println(msg);
+	}
+
+	private static void sendSlack(String webHook, List<Message> messages) {
+		String msg = "";
+		Collections.reverse(messages);
+		for (Message message : messages) {
+			msg = msg + formatMessage(message);
+		}
+
 		SlackApi api = new SlackApi(webHook);
-		api.call(new SlackMessage("#general", "Mafagafo", "```" + message + "```"));
+		api.call(new SlackMessage("#general", "Mafagafo", "```" + msg + "```"));
 	}
 
-	private static String getMessageRow(AccessKeyMetadata key, AccessKeyLastUsed used) {
+	private static String formatMessage(Message message) {
+		return String.format("%-20s\t%-20s\t%-7s\t%-35s\t%-35s\t%3d\n", message.getUserName(), message.getAccessKeyId(),
+				message.getStatus(), message.getCreateDate(), message.getLastUsedDate(), message.getDiff());
+	}
+
+	private static Message createMessage(AccessKeyMetadata key, AccessKeyLastUsed used) {
 		Date now = new Date();
 		Date lastUsed = used.getLastUsedDate();
-		
+
 		long diff = 999;
-		if(lastUsed != null){
+		if (lastUsed != null) {
 			diff = (now.getTime() - lastUsed.getTime()) / (60 * 60 * 24 * 1000);
 		}
-		
-		return String.format("%-20s\t%-20s\t%-7s\t%-35s\t%-35s\t%3d\n", key.getUserName(), key.getAccessKeyId(),
-				key.getStatus(), key.getCreateDate(), used.getLastUsedDate(), diff);
+
+		Message message = new Message(key.getUserName(), key.getAccessKeyId());
+		message.setStatus(key.getStatus());
+		message.setCreateDate(key.getCreateDate());
+		message.setLastUsedDate(used.getLastUsedDate());
+		message.setDiff(diff);
+
+		return message;
+	}
+
+	private static class Message implements Comparable{
+		private String userName;
+		private String accessKeyId;
+		private String status;
+		private Date createDate;
+		private Date lastUsedDate;
+		private long diff;
+
+		public Message(String userName, String accessKeyId) {
+			this.userName = userName;
+			this.accessKeyId = accessKeyId;
+		}
+
+		public String getUserName() {
+			return userName;
+		}
+
+		public String getAccessKeyId() {
+			return accessKeyId;
+		}
+
+		public String getStatus() {
+			return status;
+		}
+
+		public void setStatus(String status) {
+			this.status = status;
+		}
+
+		public Date getCreateDate() {
+			return createDate;
+		}
+
+		public void setCreateDate(Date createDate) {
+			this.createDate = createDate;
+		}
+
+		public Date getLastUsedDate() {
+			return lastUsedDate;
+		}
+
+		public void setLastUsedDate(Date lastUsedDate) {
+			this.lastUsedDate = lastUsedDate;
+		}
+
+		public long getDiff() {
+			return diff;
+		}
+
+		public void setDiff(long diff) {
+			this.diff = diff;
+		}
+
+		@Override
+		public int compareTo(Object o) {
+			Message other = (Message) o;
+			return (int) (this.diff - other.getDiff());
+		}
 	}
 }
